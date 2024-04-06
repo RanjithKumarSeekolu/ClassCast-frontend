cd540101890977204e61f3487937cd253f9c7c60
ranjith <ranjithkumarseekolu@gmail.com>
2024-04-06 16:19:55 +0530

    Add routings, landing page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import "./App.css";
 import MonacoEditor from "./components/MonacoEditor";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import Header from "./components/Header";
 
 function App() {
-  return <MonacoEditor />;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            index
+            element={
+              <Header>
+                <LandingPage />
+              </Header>
+            }
+          />
+          <Route path="/class" element={<MonacoEditor />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
