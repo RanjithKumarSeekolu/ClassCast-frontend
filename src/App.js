687c2f2c575e43f6f6dbb6239bca480010394db3
ranjith <ranjithkumarseekolu@gmail.com>
2024-04-13 09:55:43 +0530

    Add socket.io

--- src/App.js
+++ src/App.js
@@ -3,24 +3,27 @@ import MonacoEditor from "./components/MonacoEditor";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import Header from "./components/Header";
+import { WebsocketProvider } from "./context/SocketContext";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            index
-            element={
-              <Header>
-                <LandingPage />
-              </Header>
-            }
-          />
-          <Route path="/class" element={<MonacoEditor />} />
-        </Routes>
-      </BrowserRouter>
+      <WebsocketProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              index
+              element={
+                <Header>
+                  <LandingPage />
+                </Header>
+              }
+            />
+            <Route path="/class" element={<MonacoEditor />} />
+          </Routes>
+        </BrowserRouter>
+      </WebsocketProvider>
     </>
   );
 }
